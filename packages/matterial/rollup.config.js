6f620825c275bece11fff7cc00ccefbda6cc599b
Matt Berti <mat.berti@gmail.com>
2022-03-20 16:39:46 +0800

    Rollup config and test package

---
+++ packages/matterial/rollup.config.js
@@ -0,0 +1,31 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import sassPlugin from 'rollup-plugin-sass'
+import sass from 'sass'
+
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.tsx',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+      strict: false,
+    },
+  ],
+  plugins: [
+    typescript(),
+    nodeResolve(),
+    commonjs(),
+    sassPlugin({
+      runtime: sass,
+      insert: false,
+      output: './dist/global.css', // Will need to be imported into the app manually, eg `import './node_modules/mtui/global.css'`
+    }),
+  ],
+  external: ['react', 'react-dom', 'react/jsx-runtime'],
+}
---
+++ packages/matterial/src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+const Package: React.FC = () => (
+  <div className="surface">
+    <h2 style={{ color: 'var(--color-primary)' }}>Matterial UI</h2>
+    <p>A design system by Matt Berti</p>
+  </div>
+)
+
+export default Package

956639c7973ac42405bc396aa98969e16e3c27b0
Matt Berti <mat.berti@gmail.com>
2022-03-26 23:36:25 +0800

    Webpack config

--- packages/matterial/src/components/index.ts
+++ packages/matterial/src/components/index.ts
@@ -1,14 +1,14 @@
 // export { default as Alert, useAlert } from './Alert'
-export { default as Article } from './Article'
-export { default as Avatar, AvatarGroup } from './Avatar'
+// export { default as Article } from './Article'
+// export { default as Avatar, AvatarGroup } from './Avatar'
 // export { default as Badge } from './Badge'
-export { default as Button, IconButton } from './Button'
+// export { default as Button, IconButton } from './Button'
 // export { default as DateTime } from './DateTime'
 // export { default as Dialog, CloseButton, useDialog } from './Dialog'
 // export { default as Divider } from './Divider'
 // export { Form, FormGroup, SubmitRow, TextInput, useForm } from './Form'
 // export { default as Link } from './Link'
-export { default as Loader } from './Loader'
+// export { default as Loader } from './Loader'
 // export {
 //   Menu,
 //   MenuList,

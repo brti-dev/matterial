818b1f0086f0367893ed66d2ff10dec75fbf2cfd
Matt Berti <mat.berti@gmail.com>
2022-03-27 12:25:57 +0800

    update CheckButton tests

--- packages/matterial/src/components/CheckButton/check-button.test.tsx
+++ packages/matterial/src/components/CheckButton/check-button.test.tsx
@@ -1,16 +1,18 @@
 import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
+import renderer from 'react-test-renderer'
 
 import { render, screen } from '../../../test-utils'
-import CheckButton, { CheckButtonGroup } from '.'
-import userEvent from '@testing-library/user-event'
+import { CheckButton, CheckButtonGroup } from '.'
 
-const CheckButtons = ({ checked, setChecked = () => {} }: any) => (
+const CheckButtons = ({ checked, setChecked = () => {}, ...rest }: any) => (
   <>
     <CheckButton
       name="daddy"
       value="true"
       checked={checked == 'daddy'}
       onChange={() => setChecked('daddy')}
+      {...rest}
     >
       I'm a daddy
     </CheckButton>
@@ -19,58 +21,74 @@ const CheckButtons = ({ checked, setChecked = () => {} }: any) => (
       value="true"
       checked={checked == 'has_boat'}
       onChange={() => setChecked('has_boat')}
+      {...rest}
     >
       I have a boat
     </CheckButton>
   </>
 )
 
-test('should render group vertically', () => {
-  const id = 'check-button-group'
-
-  const { getByTestId } = render(
-    <CheckButtonGroup orientation="vertical" data-testid={id}>
-      <CheckButtons />
-    </CheckButtonGroup>
-  )
-
-  expect(getByTestId(id)).toHaveClass('containerVertical')
+test('should render correctly', () => {
+  const tree = renderer
+    .create(
+      <CheckButtonGroup orientation="vertical" className="cb__group">
+        <CheckButtons checked="daddy" className="cb__item" />
+      </CheckButtonGroup>
+    )
+    .toJSON()
+  expect(tree).toMatchSnapshot()
 })
 
-test('should render group as an overloaded element', () => {
-  const id = 'check-button-group'
-  const Row = ({ id }: { id: string }) => (
-    <span data-testid={id} className="foo" />
-  )
+describe('check button', () => {
+  test('should be checked when set as `checked`', () => {
+    const { getAllByRole } = render(
+      <CheckButtonGroup>
+        <CheckButtons checked="daddy" />
+      </CheckButtonGroup>
+    )
 
-  const { getByTestId } = render(
-    <CheckButtonGroup className="bar" as={Row} id={id}>
-      <CheckButtons />
-    </CheckButtonGroup>
-  )
+    const checkBox = getAllByRole('checkbox')[0]
+    expect(checkBox).toBeChecked()
+  })
 
-  expect(getByTestId(id)).toHaveClass('foo')
+  test('should execute callback when clicked', () => {
+    let checked = false
+    render(
+      <CheckButtonGroup>
+        <CheckButtons setChecked={() => (checked = true)} />
+      </CheckButtonGroup>
+    )
+
+    userEvent.click(screen.getByText("I'm a daddy"))
+    expect(checked).toBeTruthy()
+  })
 })
 
-test('should be checked when set as `checked`', () => {
-  const { getAllByRole } = render(
-    <CheckButtonGroup>
-      <CheckButtons checked="daddy" />
-    </CheckButtonGroup>
-  )
+describe('check button group', () => {
+  test('should render vertically', () => {
+    const id = 'check-button-group'
 
-  const checkBox = getAllByRole('checkbox')[0]
-  expect(checkBox).toBeChecked()
-})
+    const { getByTestId } = render(
+      <CheckButtonGroup orientation="vertical" data-testid={id}>
+        <CheckButtons />
+      </CheckButtonGroup>
+    )
 
-test('should execute callback when clicked', () => {
-  let checked = false
-  render(
-    <CheckButtonGroup>
-      <CheckButtons setChecked={() => (checked = true)} />
-    </CheckButtonGroup>
-  )
+    expect(getByTestId(id)).toHaveClass('containerVertical')
+  })
 
-  userEvent.click(screen.getByText("I'm a daddy"))
-  expect(checked).toBeTruthy()
+  test('should render as an overloaded element', () => {
+    const id = 'check-button-group'
+    const Row = ({ id }: { id: string }) => (
+      <span data-testid={id} className="foo" />
+    )
+
+    const { getByTestId } = render(
+      <CheckButtonGroup className="bar" as={Row} id={id}>
+        <CheckButtons />
+      </CheckButtonGroup>
+    )
+
+    expect(getByTestId(id)).toHaveClass('foo')
+  })
 })

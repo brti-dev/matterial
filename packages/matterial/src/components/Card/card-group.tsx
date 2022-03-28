f2922db6f2134d3a330072f3b2c0df03a0e1762e
Matt Berti <mat.berti@gmail.com>
2022-03-28 20:05:55 +0800

    v1.1 Card component + styles cleanup

---
+++ packages/matterial/src/components/Card/card-group.tsx
@@ -0,0 +1,24 @@
+import classnames from 'lib/classnames'
+import 'card.scss'
+
+export type CardGroupProps = {
+  className?: string
+  orientation?: 'horizontal' | 'vertical'
+  wrap?: boolean
+}
+
+export function CardGroup({
+  className,
+  orientation = 'horizontal',
+  wrap = true,
+  ...rest
+}: CardGroupProps) {
+  const classNames = classnames(
+    className,
+    'cardgroup',
+    `cardgroup__orientation--${orientation}`,
+    `cardgroup__wrap--${!wrap ? 'nowrap' : 'wrap'}`
+  )
+
+  return <div className={classNames} {...rest} />
+}
---
+++ packages/matterial/src/components/Card/card.tsx
@@ -0,0 +1,18 @@
+import classnames from 'lib/classnames'
+import {
+  OverloadedElement,
+  OverloadedElementProps,
+} from 'interfaces/OverloadedElement'
+
+export type CardProps = OverloadedElementProps & {
+  children: React.ReactNode
+  className?: string
+}
+
+export const CardGroup: OverloadedElement<CardProps> = ({
+  as: Component = 'div',
+  className,
+  ...rest
+}: CardProps) => {
+  return <Component className={classnames(className, 'card')} {...rest} />
+}

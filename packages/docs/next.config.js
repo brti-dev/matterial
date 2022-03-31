9c01116953e14cde59267a36677d13eb745cf05e
Matt Berti <mat.berti@gmail.com>
2022-03-31 21:09:54 +0800

    docs config updated

--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -1,18 +1,3 @@
-module.exports = {
-  // For static export to deploy to Dreamhost (not necessary for AWS)
-  // trailingSlash: true,
-  async headers() {
-    return [
-      {
-        source: '/:all*(svg|jpg|png|webp)',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=9999999999, must-revalidate',
-          },
-        ],
-      },
-    ]
-  },
-}
+const withTM = require('next-transpile-modules')(['../matterial'])
+
+module.exports = withTM({})

c58fdd0137828d139dc95d0044223482c06572c7
Matt Berti <mat.berti@gmail.com>
2022-04-01 17:57:18 +0800

    docs Next.js config for module transpilation

--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -1,3 +1,19 @@
+const path = require('path')
 const withTM = require('next-transpile-modules')(['../matterial'])
 
-module.exports = withTM({})
+module.exports = withTM({
+  webpack: (config, options) => {
+    if (options.isServer) {
+      config.externals = ['react', ...config.externals]
+    }
+
+    config.resolve.alias['react'] = path.resolve(
+      __dirname,
+      '.',
+      'node_modules',
+      'react'
+    )
+
+    return config
+  },
+})

4f7c79f04f56b4df011be71ae42fb7b0e946d920
Matt Berti <mat.berti@gmail.com>
2022-05-15 23:45:14 +0800

    cleanup

--- packages/matterial/src/const.ts
+++ packages/matterial/src/const.ts
@@ -1,9 +1,4 @@
 import { Color, Accent, Severity } from './interfaces/theme'
-import packageJson from '../package.json'
-
-export const PACKAGE = packageJson.name
-export const VERSION = packageJson.version
-export const DESCRIPTION = packageJson.description
 
 export const SEVERITIES: Severity[] = ['error', 'warning', 'info', 'success']
 

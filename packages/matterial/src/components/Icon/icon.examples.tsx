b1ae05364e2ed5babd3659a1f3e40235885a5a6f
Matt Berti <mat.berti@gmail.com>
2022-05-31 22:28:38 +0800

    Add Icon docs and examples

---
+++ packages/matterial/src/components/Icon/icon.examples.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Button, Container, Tooltip, useAlert } from '../index'
+import * as Icons from './icon'
+import useClipboardCopy from '../../lib/use-clipboard-copy'
+
+export function AllIcons() {
+  const [copied, copy] = useClipboardCopy()
+  const [Alert, setAlert] = useAlert()
+
+  React.useEffect(() => {
+    if (copied) {
+      setAlert({ severity: 'info', message: 'Copied to clipboard' })
+      setTimeout(() => setAlert(), 1000)
+    }
+  }, [copied])
+
+  return (
+    <Container row style={{ position: 'relative' }}>
+      {copied && (
+        <div
+          style={{
+            flex: '0 0 100%',
+            position: 'absolute',
+          }}
+        >
+          <Alert />
+        </div>
+      )}
+      {Object.keys(Icons).map(icon => (
+        <Tooltip key={icon} label={icon}>
+          <Button size="large" shape="circle" onClick={() => copy(icon)}>
+            {/* @ts-ignore */}
+            {React.createElement(Icons[icon], { size: 40 })}
+          </Button>
+        </Tooltip>
+      ))}
+    </Container>
+  )
+}

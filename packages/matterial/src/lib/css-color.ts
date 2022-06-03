f6900831d583152418d63d8eb755c8176c91f9fd
Matt Berti <mat.berti@gmail.com>
2022-06-03 13:18:01 +0800

    CSS color var will include default

--- packages/matterial/src/lib/css-color.ts
+++ packages/matterial/src/lib/css-color.ts
@@ -3,18 +3,25 @@ import { COLORS, ACCENTS } from '../const'
 /**
  * Parse color prop to CSS
  *
+ * @eg primary --> var(--color-primary)
+ * @eg hotpink --> hotpink
+ *
  * @returns {string | null} Color parsed for use in CSS
  */
 export default function cssColor(
   color?: string,
   includeAccents = true,
-  allowDefaultColor = false
+  allowDefaultColor = true
 ): string | null {
   if (!color) {
     return null
   }
 
-  const colors = [...COLORS, ...(includeAccents ? ACCENTS : [])]
+  const colors = [
+    ...COLORS,
+    ...(includeAccents ? ACCENTS : []),
+    ...(allowDefaultColor ? ['default'] : []),
+  ]
 
   if (colors.includes(color as any)) {
     return `var(--color-${color})`

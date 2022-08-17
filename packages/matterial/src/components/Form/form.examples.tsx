782cc00afe378e4555536be16fa0198f028beb31
Matt Berti <mat.berti@gmail.com>
2022-08-16 21:45:24 -0400

    Update form example with helper text

--- packages/matterial/src/components/Form/form.examples.tsx
+++ packages/matterial/src/components/Form/form.examples.tsx
@@ -32,6 +32,7 @@ export function FormExample() {
               onChange={handleChange}
             />
           }
+          helperText="Please enter your full name"
         />
         <FormGroup
           label="Feedback"

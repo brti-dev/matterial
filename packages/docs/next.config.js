36ca3f64ab9382d4661cf5a51f1c2ca9c244b5cd
Matt Berti <mat.berti@gmail.com>
2023-10-26 10:58:25 +0700

    docs upgrade next 13 and react

--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -1,25 +1,3 @@
-// const path = require('path')
-const withTM = require('next-transpile-modules')(['matterial'])
-
-module.exports =
-  withTM(/*{
-  webpack: (config, options) => {
-    if (options.isServer) {
-      config.externals = ['react', ...config.externals]
-    }
-
-    config.resolve.alias['react'] = path.resolve(
-      __dirname,
-      '.',
-      'node_modules',
-      'react'
-    )
-
-    return config
-  },
-  experimental: {
-    // Experimental feature for allowing importing Typescript files outside of
-    // the root directory
-    externalDir: true,
-  },
-}*/)
+module.exports = {
+  transpilePackages: ['matterial'],
+}

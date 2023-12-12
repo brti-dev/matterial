d15f7a4f894196f9b15e06cf989c03b48b66d6df
Matt Berti <mat.berti@gmail.com>
2023-12-12 23:01:06 +0700

    Navigation

--- packages/docs/app/setup/page.tsx
+++ packages/docs/app/setup/page.tsx
@@ -1,9 +1,8 @@
-import Link from 'next/link'
-import { Layout, Article, NavMap } from '../../../matterial/src'
-import Heading from 'components/Heading'
+import { Layout, Article } from '../../../matterial/src'
 import metadata_ from 'lib/metadata'
 import { Mdx } from 'components/Mdx'
-import { compileMdx, getDocsFiles } from 'lib/mdx'
+import { compileMdx } from 'lib/mdx'
+import navMap from 'lib_/nav-map'
 
 const source = `To begin, install the package.
 
@@ -16,8 +15,6 @@ Use Matterial's \`<Html>\` and \`<Body>\` components in your layout:
 
 import { Html, Body } from 'matterial'
 import 'src/styles/main.scss' // Your additional styles
-
-export const metadata = {} // Your metadata
 
 export default function Layout({ children }) {
   return (
@@ -33,16 +30,11 @@ Use Matterial's \`<Layout>\` component in your page:
 \`\`\`jsx
 // app/page.tsx
 
-import { Layout, NavMap } from 'matterial'
+import { Layout } from 'matterial'
 
 export default function Page() {
-  const navMap: NavMap = {
-    Foo: [<Link href="/bar">Bar</Link>, <Link href="/baz">Baz</Link>],
-    _hr: '',
-    Lorem: [<Link href="/ipsum">Ipsum</Link>],
-  }
   return (
-    <Layout nav={navMap}>
+    <Layout>
       Hello, world
     </Layout>
   )
@@ -69,18 +61,7 @@ export default function Page() {
 export const metadata = metadata_({ title: 'Matterial UI -- Getting Started' })
 
 export default async function SetupPage() {
-  const navMap: NavMap = {
-    _heading: <Heading />,
-    _: [<Link href="/ipsum">Ipsum</Link>],
-    Foo: [<Link href="/bar">Bar</Link>, <Link href="/baz">Baz</Link>],
-    _hr: '',
-    Lorem: [<Link href="/ipsum">Ipsum</Link>],
-  }
-
   const { compiledSource } = await compileMdx(source)
-  // const components = getDocsFiles().map(fileName =>
-  //   fileName.replace('.docs.mdx', '')
-  // )
 
   return (
     <Layout nav={navMap}>
---
+++ packages/docs/src/lib_/nav-map.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import Heading from 'components/Heading'
+import { NavMap } from '../../../matterial/src'
+import { capitalize, unKebabCase } from 'lib/string'
+import { getDocsFiles } from 'lib/mdx'
+
+const components = getDocsFiles().map(fileName =>
+  fileName.replace('.docs.mdx', '')
+)
+
+const navMap: NavMap = {
+  _heading: <Heading />,
+  _: [
+    <Link href="/">Homepage</Link>,
+    <Link href="/setup">Setup</Link>,
+    <Link href="/layout">Layout</Link>,
+  ],
+  Components: components.map(slug => (
+    <Link href={`/components/${slug}`} legacyBehavior>
+      {capitalize(unKebabCase(slug))}
+    </Link>
+  )),
+}
+
+export default navMap

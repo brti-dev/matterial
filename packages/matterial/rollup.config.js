0a607f39d6db83330d503ac641ff0ca17fac7ca6
Matt Berti <mat.berti@gmail.com>
2023-12-15 23:24:58 +0700

    Rollup config

--- packages/matterial/rollup.config.js
+++ packages/matterial/rollup.config.js
@@ -1,7 +1,9 @@
+import { createRequire } from 'node:module'
+
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
-import dts from 'rollup-plugin-dts'
+// import dts from 'rollup-plugin-dts'
 import postcss from 'rollup-plugin-postcss'
 import json from '@rollup/plugin-json'
 import { terser } from 'rollup-plugin-terser'
@@ -9,6 +11,29 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import copy from 'rollup-plugin-copy'
 import deleteFiles from 'rollup-plugin-delete'
 
+const require = createRequire(import.meta.url)
+const pkg = require('./package.json')
+
+/**
+ * Used for generating external dependencies
+ * Credit: Mateusz Burzyński (https://github.com/Andarist)
+ * Source: https://github.com/rollup/rollup-plugin-babel/issues/148#issuecomment-399696316
+ */
+const makeExternalPredicate = externalArr => {
+  if (externalArr.length === 0) {
+    return () => false
+  }
+  const pattern = new RegExp(`^(${externalArr.join('|')})($|/)`)
+
+  return id => pattern.test(id)
+}
+
+const external = makeExternalPredicate([
+  // Handles both dependencies and peer dependencies so we don't have to manually maintain a list
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+])
+const globals = { react: 'React' }
 const plugins = [
   peerDepsExternal(),
   resolve(),
@@ -23,6 +48,17 @@ const plugins = [
     compress: { directives: false },
   }),
 ]
+const onwarn = (warning, warn) => {
+  if (
+    warning.code === 'MODULE_LEVEL_DIRECTIVE' &&
+    warning.message.includes('use client')
+  ) {
+    // Suppress warning
+    return
+  }
+
+  warn(warning)
+}
 
 export default [
   {
@@ -31,6 +67,7 @@ export default [
       file: 'dist/index.js',
       format: 'esm',
       assetFileNames: '[name][extname]',
+      globals,
       banner: `/*
  * Matterial UI
  * https://matterial.brti.dev
@@ -40,6 +77,8 @@ export default [
 'use client';`,
     },
     plugins: [deleteFiles({ targets: 'dist/*' }), ...plugins],
+    external,
+    onwarn,
   },
   // {
   //   input: 'dist/types/index.d.ts',
@@ -49,13 +88,14 @@ export default [
   // },
   {
     input: 'src/components/examples.ts',
-    output: [
-      {
-        file: 'dist/examples.js',
-        format: 'esm',
-        sourcemap: true,
-      },
-    ],
+    output: {
+      file: 'dist/examples.js',
+      format: 'esm',
+      sourcemap: true,
+      globals,
+    },
     plugins,
+    external,
+    onwarn,
   },
 ]

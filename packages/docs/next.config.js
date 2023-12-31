3b890b7a98a705dfd90d690dfd1d151c729af9b4
Matt Berti <mat.berti@gmail.com>
2023-12-31 20:32:05 +0700

    Debug vercel build: transpile packages

--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -2,7 +2,12 @@ const withMDX = require('@next/mdx')()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['matterial'],
+  transpilePackages: [
+    '@ariakit/react',
+    'matterial',
+    'react-icons/ai',
+    'react-icons/bi',
+  ],
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below

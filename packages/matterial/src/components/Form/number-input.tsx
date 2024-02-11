cfdb58a5d60a636550f8f8de0596c6e7fb85604b
Matt Berti <mat.berti@gmail.com>
2024-02-11 13:15:17 +0700

    allow NumberInput to be uncontrolled

--- packages/matterial/src/components/Form/number-input.tsx
+++ packages/matterial/src/components/Form/number-input.tsx
@@ -6,15 +6,13 @@ export const NumberInput = React.forwardRef<
   HTMLTextAreaElement & HTMLInputElement,
   NumberInputProps
 >((props, ref) => {
-  const { name, value = 0, onChange = () => null, ...fieldProps } = props
+  const { onChange = () => null, ...fieldProps } = props
 
   return (
     <input
       {...fieldProps}
       type="number"
       ref={ref}
-      name={name}
-      value={value}
       className="input"
       onChange={onChange}
     />
